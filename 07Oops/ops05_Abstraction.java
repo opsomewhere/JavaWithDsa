a7cdf2c4347828cba56f1c6334df0e9d0970e60b
Om Prakash <omprakash908987@gmail.com>
2025-07-20 15:01:38 +0530

    feat: Implement abstraction in Java with Animal, Horse, and Chicken classes

---
+++ 07Oops/ops05_Abstraction.java
@@ -0,0 +1,29 @@
+public class ops05_Abstraction {
+    public static void main(String[] args) {
+    Horse h = new Horse();
+    h.walk();
+    h.eat();
+
+    Chicken c = new Chicken();
+    c.walk();
+    c.eat();
+
+    }
+}
+abstract class Animal{
+    void eat(){
+        System.out.println("Eating");  
+    }
+    abstract void walk();
+}
+
+class Horse extends Animal{
+    void walk(){
+        System.out.println("Walks on 4 leg");
+    }
+}
+class Chicken extends Animal{
+    void walk(){
+        System.out.println("walk on 2 leg");
+    }
+}

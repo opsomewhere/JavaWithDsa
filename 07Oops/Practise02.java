bfcffe53bcf9ed0d90538eee0d3f2ea948049e3d
Om Prakash <omprakash908987@gmail.com>
2025-09-27 16:16:26 +0530

    changes

---
+++ 07Oops/Practise02.java
@@ -0,0 +1,23 @@
+public class Practise02 {
+    public static void main(String[] args) {
+
+        //Answer a
+
+        
+        
+    }
+}
+
+class Automobile{
+    private String drive(){
+        return "Driving car";
+    }
+}
+
+class Car extends Automobile{
+    protected String drive(){
+        return "Driving child class";
+    }
+}
+
+
--- 08Recursion/ques.java
+++ 08Recursion/ques.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class ques {
+    public static void main(String[] args) {
+        String name= "Hello";
+        int j = name.length()-1;
+        for (int i = j; i >= 0; i--) {
+            System.out.println(name.charAt(i));
+        }
+    }
 }

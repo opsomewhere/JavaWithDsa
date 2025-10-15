2350b989e4fcc4d95e93b2baf4d54068d750191e
Om Prakash <omprakash908987@gmail.com>
2025-10-15 12:54:14 +0530

    feat: Implement ArrayList operations and add backtracking example

---
+++ 10Backtrack/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("BACKTRACKING");
+        // Backtracking 
+        // desigion - jab sirf pta karna ho ki rasta availale hai ya nahi t/f aur kya ek possible rasta hai 
+        // optimization - isme samse optmised path konsa ha , 
+        // Enumeration - isme ham pta ki kitni raste available 
+
+        // BACKTRAKING - ARRAY
+        
+    }
+}
